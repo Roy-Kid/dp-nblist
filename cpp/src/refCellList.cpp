ed871a789431bf6fc1b5881611539b36cfa3ffaf
okihane <30775452+okihane@users.noreply.github.com>
2023-08-31 21:56:17 +0800

    Update refCellList.cpp

--- cpp/src/refCellList.cpp
+++ cpp/src/refCellList.cpp
@@ -183,7 +183,7 @@ void NeighborList::update(std::vector<Vec3<double>> &xyz)
     }
 }
 
-NeighborListArray NeighborList::get_listArray()
+std::vector<std::vector<size_t>> NeighborList::get_listArray()
 {
     return _neighborListArray;
 }
